a97305cf1aa4b8b21517a720a39efa6d203332ca
PhuNguyen <nguyentanphu@hotmail.com>
2020-02-13 23:17:56 +0700

    finish list and add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expense/widgets/user_transaction.dart';
-import 'widgets/transaction_list.dart';
+import 'package:personal_expense/widgets/transaction_list.dart';
+
+import 'widgets/new_transaction.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,27 +10,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Personal expense',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(
+        id: 't1', title: 'Buy grocery', amount: 20.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Go to wedding', amount: 300, date: DateTime.now())
+  ];
+
+  void addNewTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+        id: DateTime.now().toIso8601String(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  void startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return NewTransaction(addNewTransaction);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal expense'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              startAddNewTransaction(context);
+            },
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('top'),
+                )),
+            TransactionList(_transactions),
+          ],
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Personal expense'),
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                      width: double.infinity,
-                      child: Card(
-                        color: Colors.blue,
-                        child: Text('top'),
-                      )),
-                  UserTransaction(),
-                ],
-              ),
-            )));
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          startAddNewTransaction(context);
+        },
+      ),
+    );
   }
 }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,41 +1,59 @@
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 import '../models/funcs.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final VoidStringDoubleFunc _addHandler;
   NewTransaction(VoidStringDoubleFunc addHandler) : _addHandler = addHandler;
 
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Card(
-                elevation: 5,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Title'),
-                        controller: titleController,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController,
-                        keyboardType: TextInputType.number,
-                      ),
-                      FlatButton(
-                        onPressed: () => _addHandler(titleController.text, double.parse(amountController.text)),
-                        child: Text(
-                          'Add transaction',
-                          style: TextStyle(color: Colors.purple),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              );
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+            ),
+            FlatButton(
+              onPressed: () {
+                if (titleController.text.isEmpty ||
+                    amountController.text.isEmpty) {
+                  Flushbar(
+                    message: 'Title and Amount could not be empty!',
+                    backgroundColor: Colors.red,
+                    duration: Duration(seconds: 3),
+                  ).show(context);
+                  return;
+                }
+                widget._addHandler(
+                    titleController.text, double.parse(amountController.text));
+              },
+              child: Text(
+                'Add transaction',
+                style: TextStyle(color: Colors.purple),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
